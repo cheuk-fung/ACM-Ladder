0a61b27d816bb0e90be6f87b96332cdb8d7fec11
Cheuk-fung Ng <osideal@gmail.com>
2013-01-22 16:34:53 +0800

    hello, ACM Ladder on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ACMLadder::Application.config.secret_token = '083c1ae61ce8d88a956e731e573894aa2c3484d1150bde72df1e9f2654efe1ad8fd7bb244a1a6f3fc47319cd8e8f816788e25c94ef550e972d609aabed999247'
