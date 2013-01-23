faf112ee71fe55e1e817e2d925946b3325ae9f1d
Cheuk-fung Ng <osideal@gmail.com>
2013-01-23 23:34:41 +0800

    Add action create to problems

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -6,4 +6,15 @@ class ProblemsController < ApplicationController
   def new
     @problem = Problem.new
   end
+
+  def create
+    @problem = Problem.new(params[:problem])
+
+    if @problem.save
+      redirect_to problems_url, :notice => "Problem was successfully created."
+    else
+      flash[:alert] = "Failed to create problem."
+      render :action => "new"
+    end
+  end
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,3 +1,4 @@
 class Problem < ActiveRecord::Base
   attr_accessible :description, :hint, :input, :level, :memory_limit, :original_id, :output, :sample_input, :sample_output, :source, :time_limit, :title
+  validates_presence_of :description, :input, :level, :memory_limit, :original_id, :output, :sample_input, :sample_output, :source, :time_limit, :title
 end
