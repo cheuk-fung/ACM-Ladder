4c0ca7cca27a878fc00b845ddbed88969ceca038
Cheuk-fung Ng <osideal@gmail.com>
2013-01-26 01:20:00 +0800

    Optimize fetch_poj

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -12,23 +12,29 @@ class Problem < ActiveRecord::Base
   end
 
   private
+
   def fetch_poj
     require "open-uri"
 
     url = "http://poj.org/problem?id=#{original_id}"
     doc = Hpricot(open(url))
-    self.title = doc.search("div.ptt").inner_html
-    if self.title == ""
-      self.title = nil
-      return
-    end
-    self.time_limit = doc.search("div.plm td")[0].inner_html[/\d+/]
-    self.memory_limit = doc.search("div.plm td")[2].inner_html[/\d+/]
-    self.description = doc.search("div.ptx")[0].inner_html
-    self.input = doc.search("div.ptx")[1].inner_html
-    self.output = doc.search("div.ptx")[2].inner_html
-    self.sample_input = doc.search("pre.sio")[0].inner_html
-    self.sample_output = doc.search("pre.sio")[1].inner_html
-    self.hint = doc.search("div.ptx")[3].inner_html if doc.search("p.pst").inner_html.include?("Hint")
+
+    result = doc.at("div.ptt")
+    return if result.nil?
+    self.title = result.inner_html
+
+    result = doc/"div.plm td"
+    self.time_limit = result[0].inner_html[/\d+/]
+    self.memory_limit = result[2].inner_html[/\d+/]
+
+    result = doc/"div.ptx"
+    self.description = result[0].inner_html
+    self.input = result[1].inner_html
+    self.output = result[2].inner_html
+    self.hint = result[3].inner_html if doc.search("p.pst").inner_html.include?("Hint")
+
+    result = doc/"pre.sio"
+    self.sample_input = result[0].inner_html
+    self.sample_output = result[1].inner_html
   end
 end
