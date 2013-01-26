c5c53b5e9da7691206a62d0e3b9bbe35d46158aa
Cheuk-fung Ng <osideal@gmail.com>
2013-01-26 17:41:56 +0800

    Add index and new pages to Submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,22 @@
 class SubmissionsController < ApplicationController
+  def index
+    @submissions = Submission.all
+  end
+
+  def new
+    @problem = Problem.find(params[:problem_id])
+    @submission = Submission.new
+  end
+
+  def create
+    @problem = Problem.find(params[:problem_id])
+    @submission = Submission.new(params[:submission])
+    @submission.problem = @problem
+
+    if @submission.save
+      redirect_to submissions_url, :notice => "Submit successfully."
+    else
+      render :action => "new"
+    end
+  end
 end
