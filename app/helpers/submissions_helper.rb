60be1c9c011cd78dd5a3a2fd3510694f98e17d4e
Cheuk-fung Ng <osideal@gmail.com>
2013-01-29 02:14:16 +0800

    Add status list

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -2,4 +2,19 @@ module SubmissionsHelper
   def language_list
     { "C" => 0, "C++" => 1, "Java" => 2, "Pascal" => 3 }
   end
+
+  def status_list
+    [
+      "Waiting",
+      "Accepted",
+      "Presentation Error",
+      "Time Limit Exceeded",
+      "Memory Limit Exceeded",
+      "Wrong Answer",
+      "Runtime Error",
+      "Output Limit Exceeded",
+      "Compile Error",
+      "System Error"
+    ]
+  end
 end
