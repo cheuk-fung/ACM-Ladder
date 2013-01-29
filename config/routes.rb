1f73d2c5823ee034c574acea64ecab8530a8c476
Cheuk-fung Ng <osideal@gmail.com>
2013-01-30 00:00:56 +0800

    Use gem "devise"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ACMLadder::Application.routes.draw do
+  devise_for :users
+
   get "home/index"
 
   resources :problems do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130128180441) do
+ActiveRecord::Schema.define(:version => 20130129155559) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -71,8 +71,21 @@ ActiveRecord::Schema.define(:version => 20130128180441) do
     t.string   "college"
     t.string   "major"
     t.string   "mobile"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
