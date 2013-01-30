1b46eecc0c7eec5542f60016d5edce8cfe47414b
Cheuk-fung Ng <osideal@gmail.com>
2013-01-30 23:59:39 +0800

    Authenticate by either handle or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,22 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :college, :handle, :level, :major, :mobile, :school, :student_id
+  validates_presence_of :handle
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+  attr_accessible :login
+
   has_many :submissions
+
+  # Overriding the following method for authenticating by either username of email
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
