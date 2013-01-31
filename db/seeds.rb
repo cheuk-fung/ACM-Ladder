547cef8bce1b057e64651ebd5bd805f328e91f7a
Cheuk-fung Ng <osideal@gmail.com>
2013-01-31 12:02:18 +0800

    Add roles and admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating roles..."
+YAML.load(ENV['ROLES']).each do |role|
+  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  puts "\tCreated role: " << role
+end
+
+puts "Creating default admin account..."
+user = User.find_or_create_by_handle(:handle => ENV['ADMIN_HANDLE'].dup,
+                                     :email => ENV['ADMIN_EMAIL'].dup,
+                                     :password => ENV['ADMIN_PASSWORD'].dup,
+                                     :password_confirmation => ENV['ADMIN_PASSWORD'].dup)
+puts "Created admin: " << user.handle
+user.add_role :admin
