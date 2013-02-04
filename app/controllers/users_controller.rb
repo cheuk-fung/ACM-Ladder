deab885ff7d752b65cb5209e71d34a9b2c0e0e92
Cheuk-fung Ng <osideal@gmail.com>
2013-02-05 02:06:18 +0800

    Fix a bug which leads to level up beyond top level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def level_up
-    if current_user.level_up
-      redirect_to problems_path, :notice => "Congratulations! You got level up!!!!"
+    if current_user.level >= Integer(ENV['MAX_LEVEL'])
+      redirect_to problems_path, :notice => "Hey, you have already reached the top level!"
+    elsif current_user.level_up
+      redirect_to problems_path, :notice => "Congratulations! You got level up!"
     else
       redirect_to problems_path, :alert => "Want to level up? Please solve all problems in this level."
     end
