bfc1942d92d396d7cc262df1ff206150b3a3dd67
Cheuk-fung Ng <osideal@gmail.com>
2013-02-05 22:49:00 +0800

    Fix a bug that shows level up button in wrong place

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -24,7 +24,7 @@ class ProblemsController < ApplicationController
       accepted = ApplicationController.helpers.status_list["Accepted"]
       accepted_status = current_user.submissions.select(:problem_id).uniq.where(:problem_id => @problems, :status => accepted);
       accepted_status.each { |submission| @status[submission.problem_id] = :accepted }
-      if accepted_status.length == @problems.length
+      if @current_level == current_user.level && accepted_status.length == @problems.length
         @level_up = true
       else
         failed_status = current_user.submissions.select(:problem_id).uniq.where(:problem_id => @problems);
