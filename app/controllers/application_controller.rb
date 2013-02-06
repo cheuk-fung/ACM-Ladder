e35f5a94565e69689b1d0f2c1fbddd15bb797962
Cheuk-fung Ng <osideal@gmail.com>
2013-02-06 19:56:12 +0800

    Add exception Handling of cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if exception.action == :new && exception.subject.class == Submission
+      redirect_to new_user_session_path, :alert => "You need to sign in or sign up before continuing."
+    else
+      redirect_to root_path, :alert => exception.message
+    end
+  end
 end
