c9b9a11d20e3a72d74cdb41b872e691081bb714e
Cheuk-fung Ng <osideal@gmail.com>
2013-02-06 16:59:37 +0800

    Change some variables to constants in poj.rb

--- lib/oj/poj.rb
+++ lib/oj/poj.rb
@@ -3,29 +3,29 @@ require 'open-uri'
 
 # TODO: handles errors
 class POJ
-  def initialize(s)
-    @base_url = "http://poj.org"
-    @login_url = "#{@base_url}/login"
-    @submit_url = "#{@base_url}/submit"
-    @handle = ENV['POJ_HANDLE']
-    @password = ENV['POJ_PASSWORD']
+  BaseURL = "http://poj.org"
+  LoginURL = "#{BaseURL}/login"
+  SubmitURL = "#{BaseURL}/submit"
+  Handle = ENV['POJ_HANDLE']
+  Password = ENV['POJ_PASSWORD']
+  Languages = { "C++" => 0, "C" => 1, "Java" => 2, "Pascal" => 3 }
 
+  def initialize(s)
     @submission = s
-    @status_url = "#{@base_url}/status?problem_id=#{s.problem.original_id}&user_id=#{@handle}"
-    languages = { "C++" => 0, "C" => 1, "Java" => 2, "Pascal" => 3 }
-    @language = languages[ApplicationController.helpers.language_list.invert[s.language]]
+    @status_url = "#{BaseURL}/status?problem_id=#{s.problem.original_id}&user_id=#{Handle}"
+    @language = Languages[ApplicationController.helpers.language_list.invert[s.language]]
   end
 
   def submit
-    uri = URI(@base_url)
+    uri = URI(BaseURL)
     Net::HTTP.start(uri.host, uri.port) do |http|
-      uri = URI(@login_url)
+      uri = URI(LoginURL)
       request = Net::HTTP::Post.new(uri.request_uri)
-      request.set_form_data({ 'user_id1' => @handle, 'password1' => @password })
+      request.set_form_data({ 'user_id1' => Handle, 'password1' => Password })
       response = http.request(request)
       cookies = response.response['set-cookie']
 
-      uri = URI(@submit_url)
+      uri = URI(SubmitURL)
       request = Net::HTTP::Post.new(uri.request_uri)
       request['Cookie'] = cookies
       request.set_form_data({ 'problem_id' => @submission.problem.original_id,
@@ -48,7 +48,7 @@ class POJ
         @submission.memory = result[4].inner_html[/\d+/]
         @submission.time = result[5].inner_html[/\d+/]
       elsif status == "Compile Error"
-        doc = Hpricot(open("#{@base_url}/showcompileinfo?solution_id=#{self.original_id}"))
+        doc = Hpricot(open("#{BaseURL}/showcompileinfo?solution_id=#{@submission.original_id}"))
         @submission.error = doc.at("pre").inner_html
       end
 
