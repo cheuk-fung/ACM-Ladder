ababda37e41f0242993179e561013bf6380d94c3
Cheuk-fung Ng <osideal@gmail.com>
2013-03-26 09:19:16 +0000

    New model and controller for site settings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130322215034) do
+ActiveRecord::Schema.define(:version => 20130326091357) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(:version => 20130322215034) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
+
+  create_table "settings", :force => true do |t|
+    t.string   "key"
+    t.text     "value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "submissions", :force => true do |t|
     t.integer  "user_id"
