28ea504ba03549dd6ca91aacebc97ef30e9ad79f
Cheuk-fung Ng <osideal@gmail.com>
2013-03-28 16:12:30 +0000

    Fix bug: cannot get failed problem in user page if accepted is empty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @solved = @user.submissions.where(:status => OJ::StatusDict["Accepted"]).uniq.pluck(:problem_id)
-    @failed = @user.submissions.where("`problem_id` NOT IN (?)", @solved).uniq.pluck(:problem_id)
+    if @solved.empty?
+      @failed = @user.submissions.select(:problem_id).uniq.pluck(:problem_id)
+    else
+      @failed = @user.submissions.where("`problem_id` NOT IN (?)", @solved).uniq.pluck(:problem_id)
+    end
     @submitted = @user.submissions.pluck(:problem_id)
   end
 end
