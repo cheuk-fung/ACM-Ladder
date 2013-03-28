52a97fedeab06d3b00755c03650e78606fea1a3e
Cheuk-fung Ng <osideal@gmail.com>
2013-03-28 11:23:34 +0000

    Do not count hidden problems' exps

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,11 +5,11 @@ class Setting < ActiveRecord::Base
     max_level = Setting.find_by_key("MAX_LEVEL").value.to_i
     max_level.times do |level|
       Setting.find_by_key("EXP_L#{level}") do |s|
-        s.value = (Problem.where("`level` <= ?", level).sum(:exp) * 3.0 / 4).ceil
+        s.value = (Problem.where(:level => 0..level).sum(:exp) * 3.0 / 4).ceil
       end.save
     end
     Setting.find_by_key("EXP_L#{max_level}") do |s|
-      s.value = Problem.where("`level` <= ?", max_level).sum(:exp)
+      s.value = Problem.where(:level => 0..max_level).sum(:exp)
     end.save
   end
 end
