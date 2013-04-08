46429f2fdfe309db3ceba3f4b80dba9e3f4bdc01
Cheuk-fung Ng <osideal@gmail.com>
2013-04-08 12:23:49 +0000

    Add link to submission detail at id on submission index

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,10 +1,10 @@
 module SubmissionsHelper
   def get_submission_status(status)
     case status
-    when "Waiting" then ""
-    when "Compiling", "Running" then "info"
-    when "Accepted" then "success"
-    when "Compile Error", "System Error" then "warning"
+    when :wait then ""
+    when :comp, :run then "info"
+    when :ac then "success"
+    when :ce, :se then "warning"
     else "important"
     end
   end
